e40d461631e234c560d8d903dc868384a0a4e085
John Hawthorn <john@stembolt.com>
2017-07-25 16:25:51 -0700

    Use spec_helper from solidus_support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,50 +5,18 @@ ENV["RAILS_ENV"] ||= "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
-require "rspec/rails"
+require 'solidus_support/extension/feature_helper'
 
-require "capybara/rspec"
-require 'capybara-screenshot/rspec'
-require 'capybara/poltergeist'
-Capybara.register_driver(:poltergeist) do |app|
-  Capybara::Poltergeist::Driver.new app, timeout: 90
-end
-Capybara.javascript_driver = :poltergeist
-Capybara.default_max_wait_time = 10
-
-require "database_cleaner"
-require "braintree"
-require "ffaker"
-
-require "spree/testing_support/factories"
-require "spree/testing_support/order_walkthrough"
+require 'braintree'
 
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
-  config.mock_with :rspec
-
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-  config.use_transactional_fixtures = false
-
-  config.include FactoryGirl::Syntax::Methods
 
   config.before :suite do
-    DatabaseCleaner.clean_with :truncation
-
     # Don't log Braintree to STDOUT.
     Braintree::Configuration.logger = Logger.new("spec/dummy/tmp/log")
-  end
-
-  config.before do
-    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
   end
 
   FactoryGirl.find_definitions
