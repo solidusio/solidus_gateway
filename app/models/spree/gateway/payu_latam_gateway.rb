fa38da0b83a7004c47aeb47efd32c1c06e6b3b72
Cesar Carruitero <cesar@mozilla.pe>
2017-09-09 10:43:50 -0500

    ensure send token and other fields to provider

--- app/models/spree/gateway/payu_latam_gateway.rb
+++ app/models/spree/gateway/payu_latam_gateway.rb
@@ -4,6 +4,7 @@ module Spree
     preference :account_id, :string
     preference :api_login, :string
     preference :api_key, :string
+    preference :public_key, :string
 
     def provider_class
       ActiveMerchant::Billing::PayuLatamGateway
@@ -17,6 +18,32 @@ module Spree
       def partial_name
         'payu_latam'
       end
+    end
+
+    def authorize(amount, credit_card, gateway_options)
+      authorization = authorization_str(credit_card)
+      provider.authorize(amount, authorization, gateway_options)
+    end
+
+    def purchase(amount, credit_card, gateway_options)
+      authorization = authorization_str(credit_card)
+      options = add_missing_fields(gateway_options, credit_card[:gateway_customer_profile_id])
+      provider.purchase(amount, authorization, options)
+    end
+
+    private
+
+    def authorization_str(credit_card)
+      "#{credit_card[:cc_type]}|#{credit_card[:gateway_payment_profile_id]}"
+    end
+
+    def add_missing_fields(options, dni_number)
+      options.merge(
+        buyer_email: options[:email],
+        buyer_name: options[:shipping_address][:name],
+        buyer_dni_number: dni_number,
+        dni_number: dni_number
+      )
     end
   end
 end
