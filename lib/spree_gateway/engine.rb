64268a9137fb1352c8288657e4a2ba9d4d63c1ad
Yechiel Kalmenson <contact@yechiel.me>
2017-09-17 14:00:56 -0400

    Add Cardknox to engine.rb

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -6,6 +6,7 @@ module SpreeGateway
         app.config.spree.payment_methods << Spree::Gateway::AuthorizeNetCim
         app.config.spree.payment_methods << Spree::Gateway::AuthorizeNet
         app.config.spree.payment_methods << Spree::Gateway::CardSave
+        app.config.spree.payment_methods << Spree::Gateway::Cardknox
         app.config.spree.payment_methods << Spree::Gateway::Eway
         app.config.spree.payment_methods << Spree::Gateway::Linkpoint
         app.config.spree.payment_methods << Spree::Gateway::Moneris
